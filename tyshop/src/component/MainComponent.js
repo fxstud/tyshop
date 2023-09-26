58c0d174915fa7f84dc584140745344d6c26afd4
fxstud <45392855+fxstud@users.noreply.github.com>
2023-09-26 23:20:21 +0200

    starting of the e-commerce app

---
+++ tyshop/src/component/MainComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './HeaderComponent'
+
+class Main extends React.Component{
+    render(){
+        return(<Header></Header>)
+    }
+}
+
+export default Main
